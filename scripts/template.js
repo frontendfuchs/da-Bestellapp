3e4cf1ea5abd130ddca0407ffe2db1dacc63dfa0
frontendfuchs <adriana.letitia@yahoo.com>
2026-06-03 02:24:49 +0200

    style(css): add layout for dishes and categorys, colors, fontsize and buttons

--- scripts/template.js
+++ scripts/template.js
@@ -2,8 +2,10 @@ function getCategoryTemplate(categoryIndex) {
   let category = menuList[categoryIndex].category;
   return /*html*/ `
         <div class="category-list">
+          <div class="categroy-list-wrapper">
             <img src="${category.src}" alt="${category.alt}"/>
             <span>${category.name}</span>
+          </div>
             <div class="content">
                 <div id="menu-item-wrapper${categoryIndex}"></div>
             </div>
@@ -14,17 +16,17 @@ function getCategoryTemplate(categoryIndex) {
 function getDishesTemplate(categoryIndex, dishesIndex) {
   let dish = menuList[categoryIndex].dishes[dishesIndex];
   return /*html*/ `
-    <div>
+      <div class="menu-item-wrapper">
           <article class="menu-item">
             <img src="${dish.src}" />
             <h2>${dish.name}</h2>
             <p>${dish.description}</p>
             <div>
-              <span>${dish.price}</span>
-              <button>x</button>
+              <span>${dish.price.toFixed(2).replace(".", ",")} €</span>
+              <button>Add to basket</button>
             </div>
           </article>
-        </div>
+   </div>
     
     `;
 }
